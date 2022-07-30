a3ebd8b551895ff42664b8ae93f320567412c859
lvminh97 <lvminh97@gmail.com>
2022-07-31 02:18:47 +0700

    Add assets folder that contains css, js and images Add Login page
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+
+// PAGES
+import Login from './pages/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
+	</Router>
+)
